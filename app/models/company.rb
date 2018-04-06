4054b58666ccabc6e3a14bc84c7b4fc3a880f93e
John Fernkas <john@johnfernkas.com>
2018-04-05 23:04:28 -0400

    Use an enum attribute to define the plan levels and set a default
    Why:
    When a `company` is created, this `enum` attribute will limit the plan levels to the 5 defined. If one is passed that isn't defined, an `ArgumentError` is passed. If a plan level isn't specified, it is assumed the company is in a trial period and will be assigned the `basic` plan level.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,10 @@
 class Company < ApplicationRecord
+    enum plan_level: [ :legacy, :custom, :basic, :plus, :growth, :enterprise ]
+
+    before_create :set_default_plan
+
+    private
+        def set_default_plan
+            self.plan_level ||= 2
+        end
 end
