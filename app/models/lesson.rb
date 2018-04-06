4d6d31fd2193eb4f02dbc00dfba4baf7410c93cd
John Fernkas <john@johnfernkas.com>
2018-04-06 00:15:01 -0400

    Validate the presence of a company's ID when creating a lesson
    Why:
    A lesson must be related to a company.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,7 @@
 class Lesson < ApplicationRecord
     belongs_to :company
-    
+
     validates :name, presence: true, format: { with: /\A[a-z0-9\s]+\Z/i }
+
+    validates :company_id, presence: true
 end
