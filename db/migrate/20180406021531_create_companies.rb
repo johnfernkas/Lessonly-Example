dd7f96c9ca0b7537fc32de4636191b7b1bed5d32
John Fernkas <john@johnfernkas.com>
2018-04-05 23:01:29 -0400

    Use an integer data type for plan_level
    Why:
    This way the model can declare an `enum` attribute for the 5 different plan types.

--- db/migrate/20180406021531_create_companies.rb
+++ db/migrate/20180406021531_create_companies.rb
@@ -3,7 +3,7 @@ class CreateCompanies < ActiveRecord::Migration[5.1]
     create_table :companies do |t|
       t.string :name
       t.datetime :trial_status
-      t.string :plan_level
+      t.integer :plan_level
 
       t.timestamps
     end
