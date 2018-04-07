ce3e91d4a5602f1f211bb4b2774c10e2adf0fdd4
John Fernkas <john@johnfernkas.com>
2018-04-06 23:12:48 -0400

    Create an alphabetize action in the companies controller
    Why:
    When this action is called, it will return a JSON object of all companies ordered alphabetically using the `Company.alphabetize` scope.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,4 +2,8 @@ class CompaniesController < ApplicationController
   def index
     render json: { data: Company.all }
   end
+
+  def alphabetize
+    render json: { data: Company.alphabetize }
+  end
 end
